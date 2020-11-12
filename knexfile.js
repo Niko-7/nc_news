e28734d88227900cb86190073c04932561665311
Niko-7 <nfloros92@gmail.com>
2020-11-12 12:02:52 +0000

    added db to heroku

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,4 @@
+const { DB_URL } = process.env;
 const ENV = process.env.NODE_ENV || 'development';
 
 const baseConfig = {
@@ -25,6 +26,14 @@ const customConfig = {
       password: 'password'
     },
   },
+  production: {
+    connection: {
+      connectionString: DB_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+  }
 };
 
 module.exports = { ...customConfig[ENV], ...baseConfig };
